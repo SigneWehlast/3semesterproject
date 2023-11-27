29ee561cb84ee02af23ab33bf32b363f003e7104
SigneWehlast <119939548+SigneWehlast@users.noreply.github.com>
2023-11-27 13:47:05 +0100

    css på forsiden

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,13 +5,58 @@ import Vinudstillere from '../components/Vinudstillere.vue';
 
 <template>
   <main>
-    <p>forside</p>
-
     <section>
-      <Program2024 />
+      <!--hero-->
     </section>
     <section>
+      <h2 class="h2_leftalign">VELKOMMEN TIL ROSÉ FESTIVAL ODENSE</h2>
+      <p>Odenses største og pinkeste rosé festival har 
+        eksisteret siden 2018 og formår hvert år at levere et 
+        brag af en festival fyldt med</p>
+      
+      <div><!--Symbol-->
+        <p>Underholdning</p>
+      </div>
+      <div><!--Symbol-->
+        <p>Socialisering & god stemning</p>
+      </div>
+      <div><!--Symbol-->
+        <p>Lækker mad & Iskold Rosé</p>
+      </div>
+      <div><!--Symbol-->
+        <p>Efterfest med DJ</p>
+      </div>
+
+      <p>Billetten til festivalen koster 499 kr., hvilket 
+        inkluderer 15 slags rosévine fra forskellige 
+        forhandlere, en stemningsfuld festivalplads. 
+        Derudover får du dit helt eget indgraverede Riedel 
+        “Vinum Extreme Rose”-glas,så dine smagninger har 
+        de bedste forudsætninger.</p>
+
+      <!--Billede af billet-->
+      <div class="btn_blue">KØB BILLET</div>
+    </section>
+
+    <section class="pinkbackground">
+      <h2>PROGRAM ROSÉ FESTIVAL 2024</h2>
+      <Program2024 />
+      <div class="btn_pink">PROGRAM</div>
+    </section>
+    <section>
+      <h2>BLIV EN DEL AF FÆLLESSKABET</h2>
+      <p>Vi glæder os utroligt meget til at fejre denne fantastiske
+        dag sammen med alle jer roséelskere.
+      </p>
+      <!--Video-->
+    </section>
+    <section class="pinkbackground">
+      <h2>MØD VORES VINUDSTILLERE</h2>
+      <p>Vi er stolte af at kunne præsentere Danmarks friskeste
+        vinaktører på Odense Rosé Festival 2024.
+      </p>
       <Vinudstillere />
+      <div class="btn_pink">LÆS MERE</div>
     </section>
   </main>
 </template>
