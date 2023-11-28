81b0f2f2d55696f205fc712813aacc7c64ec195f
SigneWehlast <119939548+SigneWehlast@users.noreply.github.com>
2023-11-28 19:56:42 +0100

    tilføjet fontawesome og fået tilmeld til at registrere til database

--- src/components/FooterRose.vue
+++ src/components/FooterRose.vue
@@ -1,23 +1,53 @@
 <script setup>
+    import { ref } from 'vue';
+
+    const emit = defineEmits(['submitted']);
+
+    const name = ref('');
+    const email = ref('');
+
+    const onSubmitForm = () => {
+        fetch('https://semesterproject-c7cbb-default-rtdb.europe-west1.firebasedatabase.app/submit.json', {
+            method: 'POST',
+            body: JSON.stringify({
+                name: name.value,
+                email: email.value
+            })
+        })
+        .then((res) => {
+            emit('submitted');
+            resetForm();
+        });
+    };
+
+    const resetForm = () => {
+        name.value = '';
+        email.value = '';
+    }
+
 </script>
 <template>
     <footer class="pinkbackground">
-        <p class="p_centeralign bold">Følg os</p>
-        <!--symboler-->
-        <p class="p_centeralign">Tilmeld vores nyhedsbrev</p>
-        <form>
-
-        </form>
-        <div class="btn pink">TILMELD</div>
+        <div class="footer_follow">
+            <p class="p_centeralign bold">Følg os</p>
+            <!--symboler-->
+            <p class="p_centeralign">Tilmeld vores nyhedsbrev</p>
+            <form class="submitform" v-on:submit.prevent="onSubmitForm">
+                <input class="inputform" type="text" v-model="name" placeholder="Navn" />
+                <input class="inputform" type="text" v-model="email" placeholder="Email" />
+            
+                <button type="submit" class="btn pink btn_form">TILMELD</button>
+            </form>
+        </div>
         <div class="footer_flexcontainer">
-            <div>
+            <div class="footer_info">
                 <p>Rosévinfestival.dk ApS</p>
                 <p>CVR-nr. 29124939</p>
                 <p>Carl Plougs Vænge 3</p>
                 <p>5230 Odense M</p>
                 <p>Tlf. 60523079</p>
             </div>
-            <div>
+            <div class="footer_find">
                 <p>Køb billet</p>
                 <p>Om festivalen</p>
                 <p>Bliv frivillig</p>
