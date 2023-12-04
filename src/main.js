31300b35d8f3f8051bc0d9beadd7ade149ab8ca3
SigneWehlast <119939548+SigneWehlast@users.noreply.github.com>
2023-12-04 09:51:48 +0100

    swiper slettet

--- src/main.js
+++ src/main.js
@@ -3,13 +3,8 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { Swiper, SwiperSlide } from 'swiper/vue';
-import 'swiper/swiper-bundle.css';
 
 const app = createApp(App)
-app.component('Swiper', Swiper);
-app.component('SwiperSlide', SwiperSlide);
-
 app.use(router)
 
 app.mount('#app')
