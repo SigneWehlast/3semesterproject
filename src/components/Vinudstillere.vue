93e0d11558ffb590486cbc51ef9d590cd0ce9290
jaco8073 <119938238+jaco8073@users.noreply.github.com>
2023-12-04 12:02:21 +0100

    Til signe fra jacob

--- src/components/Vinudstillere.vue
+++ src/components/Vinudstillere.vue
@@ -1,8 +1,14 @@
 <script setup>
 import { onMounted, ref } from 'vue';
 
-const slides = ref([]);
+const slides = ref({});
 const error = ref(null);
+
+onMounted(() => {
+  getSlides();
+});
+
+
 
 const getSlides = () => {
   fetch('https://semesterproject-c7cbb-default-rtdb.europe-west1.firebasedatabase.app/vinudstillere.json', {
@@ -10,27 +16,21 @@ const getSlides = () => {
   })
   .then((res) => res.json())
   .then((res) => {
-    console.log('Fetched data:', res);
-    if (res && res.slides) {
-      slides.value = Object.values(res.slides);
-    } else {
-      error.value = 'Slides data is undefined or null';
-    }
+    console.log(res);
+    slides.value = res;
+    console.log(slides.value)
   })
   .catch((err) => {
     console.error(err);
-    error.value = 'Could not fetch data';
+    error.value = 'Kunne ikke hente data';
   });
 };
 
-onMounted(() => {
-  getSlides();
-});
 </script>
 
 <template>
   <div class="slideshow-container">
-    <div v-for="(slide, index) in slides" :key="index" class="fade">
+    <div v-for="(slide, index) in slides" :key="index" class="customer-logos-slider">
       <div class="slide-content">
         <h3>{{ slide.title }}</h3>
       </div>
@@ -40,33 +40,5 @@ onMounted(() => {
 </template>
 
 <style scoped>
-.slideshow-container {
-  max-width: 1000px;
-  margin: auto;
-  position: relative;
-}
 
-.fade {
-  display: none;
-  position: absolute;
-  width: 100%;
-  animation: fade 1.5s ease-in-out infinite;
-}
-
-@keyframes fade {
-  from {
-    opacity: 0.4;
-  }
-  to {
-    opacity: 1;
-  }
-}
-
-.slide-content {
-  position: absolute;
-  top: 10px;
-  left: 10px;
-  z-index: 1;
-  color: white;
-}
 </style>
