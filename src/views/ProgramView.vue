cef2078149379fa5ad62e72a148b577e7cda435a
jaco8073 <119938238+jaco8073@users.noreply.github.com>
2023-12-11 10:12:57 +0100

    border program

--- src/views/ProgramView.vue
+++ src/views/ProgramView.vue
@@ -4,11 +4,13 @@ import Program2024 from '../components/Program2024.vue';
 
 <template>
   <main>
-    <h2 class="h2_program">ROSÉ FESTIVAL PROGRAM 2024</h2>
-    <p class="margin_p">Festivalen afholdes lørdag den 25. maj 2024 ved 
-      Ansgar Anlæg Odense.</p>
-    <p class="margin_p">I åbningstiden vil der være salg af mad og snacks 
-      fra vores mange boder og food trucks.</p>
+    <div class="program_crtl">
+      <h2 class="h2_program">ROSÉ FESTIVAL PROGRAM 2024</h2>
+      <p class="margin_p">Festivalen afholdes lørdag den 25. maj 2024 ved 
+        Ansgar Anlæg Odense.</p>
+      <p class="margin_p">I åbningstiden vil der være salg af mad og snacks 
+        fra vores mange boder og food trucks.</p>
+    </div>
     <Program2024 />
   </main>
 </template>
